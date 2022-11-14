8d18bcc3e340c8b8456976402706901619b1a62d
TalMizrahii <Taltalon1927@gmail.com>
2022-11-14 22:58:30 +0200

    finished version 1.0

--- ex1.c
+++ ex1.c
@@ -23,10 +23,74 @@ int is_big_endian() {
     return 0;
 }
 
-    int get_sign_magnitude(bool bitArray[8]) {
-
+/**
+ * A power calculation function to calculate the base to the power of the exponent.
+ * @param base the base of the number we want to calculate the power of.
+ * @param exponent the exponent of the number we want to calculate the power of.
+ * @return base^exponent.
+ */
+int power(int base, int exponent) {
+    // Initiating the result to 1.
+    int result = 1;
+    // Iterating the calculation as the amount of the exponent.
+    for (int i = 0; i < exponent; i++) {
+        result = result * base;
     }
+    return result;
+}
 
-    int get_two_comp(bool bitArray[8]) {
-
+int magnitude_check(bool bitArray[8]){
+    // Declaring the mask to be a constant value of 1.
+    const int MASK = 1;
+    // Declaring the sum in decimal, and a temp variable to store the numeric value of the bitarray's position.
+    int sum = 0, temp;
+    // Iterating the array as the amount of its length - 1.
+    for (int i = 0; i < 7; i++) {
+        // Storing the numeric value of the bitarray's position.
+        temp = bitArray[i] & MASK;
+        // multiplying the temp by 2 to the power of i's position in the array and adding it to the sum.
+        sum += temp * power(2, i);
     }
+    // Otherwise, return the positive sum.
+    return sum;
+}
+
+/**
+ * Calculating an array of boolean values representing a byte in the Sign/Magnitude notation.
+ * We use magnitude_check function to check every bit in the array (except the MSB),
+ * multiplying it by 2 to the power of it's position in the array and summing it.
+ * If the MSB is 1 (true), returning the sum as negative, if it's 0 (false), returning the sum as positive.
+ * @param bitArray array of boolean values representing a byte in the Sign/Magnitude notation.
+ * @return the number the array represent in Sign/Magnitude notation.
+ */
+int get_sign_magnitude(bool bitArray[8]) {
+    // Getting the decimal value of all bit except the MSB.
+    int sum = magnitude_check(bitArray);
+    // If the MSB is 1 (true), return the negative sum.
+    if (bitArray[7] == true) {
+        return -sum;
+    }
+    // Otherwise, return the positive sum.
+    return sum;
+}
+
+/**
+ * Calculating an array of boolean values representing a byte in the Two's complement notation.
+ * We use magnitude_check function to check every bit in the array (except the MSB),
+ * multiplying it by 2 to the power of it's position in the array and summing it.
+ * If the MSB is 1 (true), we subtract 2 to the power of the MSB's index, if it's 0 (false), we do nothing to the sum.
+ * @param bitArray array of boolean values representing a byte in the Sign/Magnitude notation.
+ * @return the decimal value the array represent in Two's complement notation.
+ */
+int get_two_comp(bool bitArray[8]) {
+    // Declaring the mask to be a constant value of 1.
+    const int MASK = 1;
+    // Checking the MSB's value and storing it in the variable temp.
+    int temp = bitArray[7] & MASK;
+    // Getting the decimal value of all bit except the MSB.
+    int sum = magnitude_check(bitArray);
+    // Subtracting the calculation of temp multiplied by 2 to the power of the MSB's index.
+    sum -= temp * power(2, 7);
+    // Returning the decimal value the array represent in Two's complement notation.
+    return sum;
+}
