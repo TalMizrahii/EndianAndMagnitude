9c1a840435036633169352fb514638eef0851db0
TalMizrahii <Taltalon1927@gmail.com>
2022-11-15 08:22:45 +0200

    finished version 1.2 changed the main to the original main.

--- main.c
+++ main.c
@@ -2,13 +2,12 @@
 #include <stdbool.h>
 #include "ex1.h"
 
-
 int main() {
     bool bitArray[] = {true, false, true, true, false, true, false, true};
 
     printf("%d\n", is_big_endian());
 
-	printf("%d\n", get_sign_magnitude(bitArray));
+    printf("%d\n", get_sign_magnitude(bitArray));
 
     printf("%d\n", get_two_comp(bitArray));
 
