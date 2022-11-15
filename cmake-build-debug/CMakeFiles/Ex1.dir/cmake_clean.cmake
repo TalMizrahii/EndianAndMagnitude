93255f400f2fb849b5ff9e1aa8b6e8ca9cdd868e
TalMizrahii <Taltalon1927@gmail.com>
2022-11-15 08:12:04 +0200

    finished version 1.1 added comments.

---
+++ cmake-build-debug/CMakeFiles/Ex1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex1.dir/ex1.c.o"
+  "CMakeFiles/Ex1.dir/ex1.c.o.d"
+  "CMakeFiles/Ex1.dir/main.c.o"
+  "CMakeFiles/Ex1.dir/main.c.o.d"
+  "Ex1"
+  "Ex1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
